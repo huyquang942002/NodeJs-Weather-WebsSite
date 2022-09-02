d8ee78d6d8522734e70c54b0028bbd69c187804e
NongQuangHuy <20110496@student.hcmute.edu.vn>
2022-09-02 11:12:36 +0700

    Update forecast

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude,longitude,callback)=>{
             }else if(body.error){
                 callback("Unable to find location!",undefined);
             }else{
-                callback(undefined , body.current.weather_descriptions + ' It is currently' + body.current.temperature + ' Degress out . There is a ' + body.current.precip + ' % chance of rain');
+                callback(undefined , body.current.weather_descriptions +','+ ' It is currently ' + body.current.temperature + ' Degress out . There is a ' + body.current.precip + ' % chance of rain'
+                +' and wind speed is ' + body.current.wind_speed + ' , Humid is ' + body.current.humidity +"%");
             }
         })
 }
